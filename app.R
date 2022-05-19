658cb5a03243fa36ed82537e5c1c3fea3d341d90
Mew233 <42139976+Mew233@users.noreply.github.com>
2022-05-18 22:19:25 -0400

    can choose the database&model; can select correct drug2&cell

--- app.R
+++ app.R
@@ -3,69 +3,110 @@ library(DT)
 library(ggplot2)
 library(dplyr)
 
-df <- read.csv("data/proba_Deepsynergy (Preuer et al., 2018)_Sanger 2022.csv")[ ,2:9]
 server <- function(input, output,session){
-    
 
-    observeEvent(c(input$model,input$dataset), {
+    
+    data <- reactive({
        file_path = paste0("data/proba_",input$model,"_",input$dataset,".csv")
         if(file_test("-f", file_path)){
-            df = read.csv(paste0("data/proba_",input$model,"_",input$dataset,".csv"))[ ,2:9]        
+            df = read.csv(paste0("data/proba_",input$model,"_",input$dataset,".csv"))[ ,-1]
         }else{
-            df <- read.csv("data/proba_Deepsynergy (Preuer et al., 2018)_Sanger 2022.csv")[ ,2:9]
+            df <- read.csv("data/proba_Deepsynergy (Preuer et al., 2018)_Sanger 2022.csv")[ ,-1]
         }
-        
+      return(df)
+    })
+
+    #https://cloud.tencent.com/developer/article/1830442
+    # data <- reactive({
+    #     #req(input$file1)
+    #     df <- read.csv(paste0("data/proba_",input$model,"_",input$dataset,".csv"))[ ,-1]
+    #     return(df)
+    # })
+    
+    output$columns1 <- renderUI({
+        selectInput("d1", label = "Drug1", choices = c('All',sort(unique(as.character(data()$Drug1)))))
+    })
+    output$columns2 <- renderUI({
+        selectInput("d2", label = "Drug2", choices = c('All',sort(unique(as.character(data()$Drug2)))))
+    })
+    output$columns3 <- renderUI({
+        selectInput("cell", label = "Cell line", choices = c('All',sort(unique(as.character(data()$DepMap_ID)))))
     })
     
-    data <- df
+    #update d2
+    observeEvent(input$d1, {
+        if(input$d1 != "All"){
+            chocies =  sort(unique(as.character(data()$Drug2[data()$Drug1==input$d1])))
+            updateSelectInput(session, "d2", "Drug2", choices = c("All",chocies))
+        } else{
+            updateSelectInput(session, "d2", "Drug2", choices = c("All",sort(unique(as.character(data()$Drug2)))))
+        }
 
-    # Update as soon as Month gets populated according to the year and month selected
+    })
+    
+    #update cell
     observeEvent(
         c(input$d1,input$d2), {
             if(input$d1 != "All"){
                 if(input$d2 != "All"){
-                    choices = sort(unique(as.character(data$DepMap_ID[data$Drug1==input$d1 & data$Drug2==input$d2])))
+                    #choices = sort(unique(as.character(data()$DepMap_ID[data()$Drug1==input$d1 & data()$Drug2==input$d2])))
+                    temp = data() %>% filter(Drug1 == input$d1 | Drug2 == input$d1)  %>% filter(Drug1 == input$d2 | Drug2 == input$d2)
+                    choices = sort(unique(as.character(temp$DepMap_ID)))
                     if (length(choices)>0){
                         updateSelectInput(session, "cell", "Cell line", choices = c('All',choices))
                     }else{
                         updateSelectInput(session, "cell", "Cell line", choices = "")
                     }
-                    
+
                 }else{
-                    chocies =  sort(unique(as.character(data$DepMap_ID[data$Drug1==input$d1])))
-                    updateSelectInput(session, "cell", "Cell line", choices = c("All",chocies))    
+                    chocies =  sort(unique(as.character(data()$DepMap_ID[data()$Drug1==input$d1])))
+                    updateSelectInput(session, "cell", "Cell line", choices = c("All",chocies))
                     }
 
             }else{
                 if(input$d2 != "All"){
-                    chocies =  sort(unique(as.character(data$DepMap_ID[data$Drug2==input$d2])))
-                    updateSelectInput(session, "cell", "Cell line", choices = c("All",chocies))   
+                    chocies =  sort(unique(as.character(data()$DepMap_ID[data()$Drug2==input$d2])))
+                    updateSelectInput(session, "cell", "Cell line", choices = c("All",chocies))
                 }else{
-                    updateSelectInput(session, "cell", "Cell line", choices = c('All', sort(unique(as.character(data$DepMap_ID)))))
+                    updateSelectInput(session, "cell", "Cell line", choices = c('All', sort(unique(as.character(data()$DepMap_ID)))))
                 }
 
             }
-            
-            
+
         })
+
     
     output$table <- renderDataTable(DT::datatable({
-        data <- df
-        if (input$d1 != "All") {
-            data <- data %>% filter(Drug1 == input$d1 | Drug2 == input$d1)
-        }
-        if (input$d2 != "All") {
-            data <- data %>% filter(Drug1 == input$d2 | Drug2 == input$d2)
-        }
-        
-        if (input$cell != "All") {
-            data <- data[data$DepMap_ID == input$cell,]
-        } 
-        data
-    }))
-    
+            data <- data()
+            a = (input$d1 != "All") 
+            b = (input$d2 != "All")
+            c = (input$cell != "All")
+            if (a & b & c){ data <- data %>% filter(Drug1 == input$d1 | Drug2 == input$d1)  %>% filter(Drug1 == input$d2 | Drug2 == input$d2) %>% filter(DepMap_ID == input$cell) }
+            if (!a & !b & c){ data <- data %>% filter(DepMap_ID == input$cell) }
+            if (a & !b & !c){ data <- data %>% filter(Drug1 == input$d1 | Drug2 == input$d1) }
+            if (!a & b & !c){ data <- data %>% filter(Drug1 == input$d2 | Drug2 == input$d2) }
+            if (!a & b & c){ data <- data %>% filter(Drug1 == input$d2 | Drug2 == input$d2 , DepMap_ID == input$cell) }
+            if (a & !b & c){ data <- data %>% filter(Drug1 == input$d1 | Drug2 == input$d1 , DepMap_ID == input$cell) }
+            if (a & b & !c){ data <- data %>% filter(Drug1 == input$d1 | Drug2 == input$d1)  %>% filter(Drug1 == input$d2 | Drug2 == input$d2) }
+            if (!a & !b & !c){data<- data}
+            
+            # if (input$d1 != "All") {
+            #     data <- data %>% filter(Drug1 == input$d1 | Drug2 == input$d1)
+            #     
+            # }
+            # if (input$d2 != "All") {
+            #     data <- data %>% filter(Drug1 == input$d2 | Drug2 == input$d2)
+            # }
+            # 
+            # if (input$cell != "All") {
+            #     data <- data %>% filter(DepMap_ID == input$cell)
+            # }
+            data
+        }))
+
+
     output$heatImage <- renderPlot({
-        data <-  df
+        data <-  data()
         # Create auxiliary data.frame.
         data <- data[data$DepMap_ID == input$cell,]
         data$Var1 = factor(data$Drug1)
@@ -73,6 +114,9 @@ server <- function(input, output,session){
         frames = data %>% filter(Drug1 == input$d1, Drug2 == input$d2)
         frames$Var1 = as.integer(frames$Var1)
         frames$Var2 = as.integer(frames$Var2)
+        frames$Proba = (frames$Proba > 0.5)
+        frames$Actuals = frames$Actuals
+        frames %>% mutate(Color = ifelse(Proba == Actuals, "green", "red"))
         
         frames1 = data %>% filter(Drug1 == input$d2, Drug2 == input$d1)
         frames1$Var11 = as.integer(frames1$Var2)
@@ -80,13 +124,14 @@ server <- function(input, output,session){
         
         if (input$cell == "All") {
         }else{
-            p = ggplot( data, aes(x=Drug1,y=Drug2))
-            p +labs(title=input$cell) + theme(axis.text.x=element_text(angle=45,hjust=1)) + (
-                scale_fill_gradient2(low = "#0072B2", high = "#D55E00",,midpoint=0.5)) +
-                (geom_raster(aes(fill = Proba_deepsynergy_preuer))) +
-                geom_rect(data=frames, size=1, fill=NA, colour="black",
+            options(repr.plot.width = 12, repr.plot.height = 12)
+            p = ggplot(data, aes(x=Drug1,y=Drug2))
+            p +labs(title=input$cell) + theme(axis.text.x=element_text(size=10,angle=45,hjust=1), axis.text.y=element_text(size=6)) +scale_y_discrete(labels = abbreviate) + (
+                scale_fill_gradient2(low = "#0072B2", high = "#D55E00",midpoint=0.5)) +
+                (geom_raster(aes(fill = Proba))) +
+                geom_rect(data=frames, size=1, fill=NA, colour="green", #需要修改
                           aes(xmin=Var1 - 0.5, xmax=Var1 + 0.5, ymin=Var2 - 0.5, ymax=Var2 + 0.5))  +
-                geom_rect(data=frames1, size=1, fill=NA, colour="black",
+                geom_rect(data=frames1, size=1, fill=NA, colour="green",
                           aes(xmin=Var21 - 0.5, xmax=Var21 + 0.5, ymin=Var11 - 0.5, ymax=Var11 + 0.5))
         }
         
@@ -110,13 +155,14 @@ ui <- fluidPage(
     titlePanel('SynergyY Shiny R'),
     sidebarLayout(
         sidebarPanel(
-            selectInput("d1", "Drug1", choices = c('All', unique(as.character(df$Drug1)))),
-            selectInput("d2", "Drug2", choices = c('All', unique(as.character(df$Drug2)))),
-            selectInput("cell", "Cell line", choices = c('All', unique(as.character(df$DepMap_ID)))),
+    
             radioButtons('model','Prediction model', choices = c('LR','XGBOOST','RF','ERT','Deepsynergy (Preuer et al., 2018)','Multitaskdnn (Kim et al., 2021)',
                         'Matchmaker (Brahim et al., 2021)','Deepdds (Wang et al., 2021)','TGSynergy (Zhu et al., 2022)'),selected='Deepsynergy (Preuer et al., 2018)'),
-            radioButtons('dataset','Dataset', choices = c('DrugComb v1.5','Sanger 2022'), selected='Sanger 2022')),
-        
+            uiOutput('columns1'),
+            uiOutput('columns2'),
+            uiOutput('columns3'),
+            radioButtons('dataset','Dataset', choices = c('DrugComb v1.5','Sanger 2022'), selected='DrugComb v1.5'),
+            ),
         
         mainPanel(
             tabsetPanel(
